8a812bcb15348ee8d4b80f27455d7673203bf013
Aleksander Morgado <aleksander@lanedo.com>
2012-04-19 18:31:21 +0200

    utils: new helper method to get a printable string from a binary stream

---
+++ src/qmi-utils.h
@@ -0,0 +1,39 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ * libqmi-glib -- GLib/GIO based library to control QMI devices
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ * Copyright (C) 2012 Aleksander Morgado <aleksander@lanedo.com>
+ */
+
+/* NOTE: this is a private non-installable header */
+
+#ifndef _LIBQMI_GLIB_QMI_UTILS_H_
+#define _LIBQMI_GLIB_QMI_UTILS_H_
+
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+gchar *qmi_utils_str_hex (gpointer mem,
+                          gsize    size,
+                          gchar    delimiter);
+
+G_END_DECLS
+
+#endif /* _LIBQMI_GLIB_QMI_UTILS_H_ */
