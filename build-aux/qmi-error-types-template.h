e9549b086d2db9b73188af142da4e6f3b40f3e0d
Aleksander Morgado <aleksander@lanedo.com>
2012-05-07 14:36:30 +0200

    build-aux: generate new nick string getters for the error codes

--- build-aux/qmi-error-types-template.h
+++ build-aux/qmi-error-types-template.h
@@ -15,6 +15,7 @@ GQuark @enum_name@_quark    (void);
 GType  @enum_name@_get_type (void) G_GNUC_CONST;
 #define @ENUMNAME@ (@enum_name@_quark ())
 #define @ENUMPREFIX@_TYPE_@ENUMSHORT@ (@enum_name@_get_type ())
+const gchar *@enum_name@_get_string (@EnumName@ val);
 /*** END value-header ***/
 
 /*** BEGIN file-tail ***/
