b70fb23f5ef966488062b82701804bb916b28eac
Aleksander Morgado <aleksander@lanedo.com>
2013-01-14 16:22:02 +0100

    libmbim-glib: setup compilation of the library

---
+++ libmbim-glib/libmbim-glib.h
@@ -0,0 +1,30 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * libmbim-glib -- GLib/GIO based library to control MBIM devices
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ * Copyright (C) 2013 Aleksander Morgado <aleksander@gnu.org>
+ */
+
+#ifndef _LIBMBIM_GLIB_H_
+#define _LIBMBIM_GLIB_H_
+
+#define __LIBMBIM_GLIB_H_INSIDE__
+
+/* libmbim-glib headers */
+
+#endif /* _LIBMBIM_GLIB_H_ */
