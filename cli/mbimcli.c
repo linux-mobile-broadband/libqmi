ffb6624eb85eaaee17f1f3f12976690357ec3989
Aleksander Morgado <aleksander@lanedo.com>
2013-01-18 16:24:57 +0100

    cli: setup new 'mbimcli' to access the MBIM device from the commandline

---
+++ cli/mbimcli.c
@@ -0,0 +1,303 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * mbimcli -- Command line interface to control MBIM devices
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2013 Aleksander Morgado <aleksander@gnu.org>
+ */
+
+#include "config.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+#include <glib.h>
+#include <glib/gprintf.h>
+#include <gio/gio.h>
+
+#include <libmbim-glib.h>
+
+#include "mbimcli.h"
+
+#define PROGRAM_NAME    "mbimcli"
+#define PROGRAM_VERSION PACKAGE_VERSION
+
+/* Globals */
+static GMainLoop *loop;
+static GCancellable *cancellable;
+static MbimDevice *device;
+static gboolean operation_status;
+
+/* Main options */
+static gchar *device_str;
+static gboolean verbose_flag;
+static gboolean silent_flag;
+static gboolean version_flag;
+
+static GOptionEntry main_entries[] = {
+    { "device", 'd', 0, G_OPTION_ARG_STRING, &device_str,
+      "Specify device path",
+      "[PATH]"
+    },
+    { "verbose", 'v', 0, G_OPTION_ARG_NONE, &verbose_flag,
+      "Run action with verbose logs, including the debug ones",
+      NULL
+    },
+    { "silent", 0, 0, G_OPTION_ARG_NONE, &silent_flag,
+      "Run action with no logs; not even the error/warning ones",
+      NULL
+    },
+    { "version", 'V', 0, G_OPTION_ARG_NONE, &version_flag,
+      "Print version",
+      NULL
+    },
+    { NULL }
+};
+
+static void
+signals_handler (int signum)
+{
+    if (cancellable) {
+        /* Ignore consecutive requests of cancellation */
+        if (!g_cancellable_is_cancelled (cancellable)) {
+            g_printerr ("%s\n",
+                        "cancelling the operation...\n");
+            g_cancellable_cancel (cancellable);
+        }
+        return;
+    }
+
+    if (loop &&
+        g_main_loop_is_running (loop)) {
+        g_printerr ("%s\n",
+                    "cancelling the main loop...\n");
+        g_main_loop_quit (loop);
+    }
+}
+
+static void
+log_handler (const gchar *log_domain,
+             GLogLevelFlags log_level,
+             const gchar *message,
+             gpointer user_data)
+{
+    const gchar *log_level_str;
+    time_t now;
+    gchar time_str[64];
+    struct tm *local_time;
+    gboolean err;
+
+    /* Nothing to do if we're silent */
+    if (silent_flag)
+        return;
+
+    now = time ((time_t *) NULL);
+    local_time = localtime (&now);
+    strftime (time_str, 64, "%d %b %Y, %H:%M:%S", local_time);
+    err = FALSE;
+
+    switch (log_level) {
+    case G_LOG_LEVEL_WARNING:
+        log_level_str = "-Warning **";
+        err = TRUE;
+        break;
+
+    case G_LOG_LEVEL_CRITICAL:
+    case G_LOG_FLAG_FATAL:
+    case G_LOG_LEVEL_ERROR:
+        log_level_str = "-Error **";
+        err = TRUE;
+        break;
+
+    case G_LOG_LEVEL_DEBUG:
+        log_level_str = "[Debug]";
+        break;
+
+    default:
+        log_level_str = "";
+        break;
+    }
+
+    if (!verbose_flag && !err)
+        return;
+
+    g_fprintf (err ? stderr : stdout,
+               "[%s] %s %s\n",
+               time_str,
+               log_level_str,
+               message);
+}
+
+static void
+print_version_and_exit (void)
+{
+    g_print ("\n"
+             PROGRAM_NAME " " PROGRAM_VERSION "\n"
+             "Copyright (2013) Aleksander Morgado\n"
+             "License GPLv2+: GNU GPL version 2 or later <http://gnu.org/licenses/gpl-2.0.html>\n"
+             "This is free software: you are free to change and redistribute it.\n"
+             "There is NO WARRANTY, to the extent permitted by law.\n"
+             "\n");
+    exit (EXIT_SUCCESS);
+}
+
+/*****************************************************************************/
+/* Running asynchronously */
+
+void
+mbimcli_async_operation_done (gboolean reported_operation_status)
+{
+    /* Keep the result of the operation */
+    operation_status = reported_operation_status;
+
+    if (cancellable) {
+        g_object_unref (cancellable);
+        cancellable = NULL;
+    }
+
+    g_main_loop_quit (loop);
+}
+
+static void
+device_close_ready (MbimDevice   *dev,
+                    GAsyncResult *res)
+{
+    GError *error = NULL;
+
+    if (!mbim_device_close_finish (dev, res, &error)) {
+        g_printerr ("error: couldn't close device: %s", error->message);
+        g_error_free (error);
+    } else
+        g_debug ("Device closed");
+
+    g_main_loop_quit (loop);
+
+    /* We're done now */
+    mbimcli_async_operation_done (TRUE);
+}
+
+static void
+device_open_ready (MbimDevice   *dev,
+                   GAsyncResult *res)
+{
+    GError *error = NULL;
+
+    if (!mbim_device_open_finish (dev, res, &error)) {
+        g_printerr ("error: couldn't open the MbimDevice: %s\n",
+                    error->message);
+        exit (EXIT_FAILURE);
+    }
+
+    g_debug ("MBIM Device at '%s' ready",
+             mbim_device_get_path_display (dev));
+
+
+    /* TODO: actions here */
+
+    /* Close the device */
+    mbim_device_close (device,
+                       15,
+                       cancellable,
+                       (GAsyncReadyCallback) device_close_ready,
+                       NULL);
+}
+
+static void
+device_new_ready (GObject      *unused,
+                  GAsyncResult *res)
+{
+    GError *error = NULL;
+
+    device = mbim_device_new_finish (res, &error);
+    if (!device) {
+        g_printerr ("error: couldn't create MbimDevice: %s\n",
+                    error->message);
+        exit (EXIT_FAILURE);
+    }
+
+    /* Open the device */
+    mbim_device_open (device,
+                      15,
+                      cancellable,
+                      (GAsyncReadyCallback) device_open_ready,
+                      NULL);
+}
+
+/*****************************************************************************/
+
+int main (int argc, char **argv)
+{
+    GError *error = NULL;
+    GFile *file;
+    GOptionContext *context;
+
+    setlocale (LC_ALL, "");
+
+    g_type_init ();
+
+    /* Setup option context, process it and destroy it */
+    context = g_option_context_new ("- Control MBIM devices");
+    g_option_context_add_main_entries (context, main_entries, NULL);
+    if (!g_option_context_parse (context, &argc, &argv, &error)) {
+        g_printerr ("error: %s\n",
+                    error->message);
+        exit (EXIT_FAILURE);
+    }
+	g_option_context_free (context);
+
+    if (version_flag)
+        print_version_and_exit ();
+
+    g_log_set_handler (NULL, G_LOG_LEVEL_MASK, log_handler, NULL);
+    g_log_set_handler ("Mbim", G_LOG_LEVEL_MASK, log_handler, NULL);
+    if (verbose_flag)
+        mbim_utils_set_traces_enabled (TRUE);
+
+    /* No device path given? */
+    if (!device_str) {
+        g_printerr ("error: no device path specified\n");
+        exit (EXIT_FAILURE);
+    }
+
+    /* Build new GFile from the commandline arg */
+    file = g_file_new_for_commandline_arg (device_str);
+
+    /* Setup signals */
+    signal (SIGINT, signals_handler);
+    signal (SIGHUP, signals_handler);
+    signal (SIGTERM, signals_handler);
+
+    /* Create requirements for async options */
+    cancellable = g_cancellable_new ();
+    loop = g_main_loop_new (NULL, FALSE);
+
+    /* Launch MbimDevice creation */
+    mbim_device_new (file,
+                     cancellable,
+                     (GAsyncReadyCallback)device_new_ready,
+                     NULL);
+    g_main_loop_run (loop);
+
+    if (cancellable)
+        g_object_unref (cancellable);
+    if (device)
+        g_object_unref (device);
+    g_main_loop_unref (loop);
+    g_object_unref (file);
+
+    return (operation_status ? EXIT_SUCCESS : EXIT_FAILURE);
+}
