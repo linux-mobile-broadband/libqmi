775b00e7d0858b36e90db6e72b7c24f31070e67e
Aleksander Morgado <aleksander@lanedo.com>
2013-04-14 12:56:54 +0200

    libmbim-glib,test: unit tests for the 'Connect' message builder

--- libmbim-glib/test/test-message-command-builder.c
+++ libmbim-glib/test/test-message-command-builder.c
@@ -188,12 +188,175 @@ test_message_command_builder_set_pin (void)
     mbim_message_unref (message);
 }
 
+static void
+test_message_command_builder_raw_set_connect (void)
+{
+    MbimMessage *message;
+    MbimMessageCommandBuilder *builder;
+    const guint8 expected_message [] = {
+        /* header */
+        0x03, 0x00, 0x00, 0x00, /* type */
+        0x7C, 0x00, 0x00, 0x00, /* length */
+        0x01, 0x00, 0x00, 0x00, /* transaction id */
+        /* fragment header */
+        0x01, 0x00, 0x00, 0x00, /* total */
+        0x00, 0x00, 0x00, 0x00, /* current */
+        /* command_message */
+        0xA2, 0x89, 0xCC, 0x33, /* service id */
+        0xBC, 0xBB, 0x8B, 0x4F,
+        0xB6, 0xB0, 0x13, 0x3E,
+        0xC2, 0xAA, 0xE6, 0xDF,
+        0x0C, 0x00, 0x00, 0x00, /* command id */
+        0x01, 0x00, 0x00, 0x00, /* command_type */
+        0x4C, 0x00, 0x00, 0x00, /* buffer_length */
+        /* information buffer */
+        0x01, 0x00, 0x00, 0x00, /* session id */
+        0x01, 0x00, 0x00, 0x00, /* activation command */
+        0x3C, 0x00, 0x00, 0x00, /* access string offset */
+        0x10, 0x00, 0x00, 0x00, /* access string size */
+        0x00, 0x00, 0x00, 0x00, /* username offset */
+        0x00, 0x00, 0x00, 0x00, /* username size */
+        0x00, 0x00, 0x00, 0x00, /* password offset */
+        0x00, 0x00, 0x00, 0x00, /* password size */
+        0x00, 0x00, 0x00, 0x00, /* compression */
+        0x01, 0x00, 0x00, 0x00, /* auth protocol */
+        0x01, 0x00, 0x00, 0x00, /* ip type */
+        0x7E, 0x5E, 0x2A, 0x7E, /* context type */
+        0x4E, 0x6F, 0x72, 0x72,
+        0x73, 0x6B, 0x65, 0x6E,
+        0x7E, 0x5E, 0x2A, 0x7E,
+        /* data buffer */
+        0x69, 0x00, 0x6E, 0x00, /* access string */
+        0x74, 0x00, 0x65, 0x00,
+        0x72, 0x00, 0x6E, 0x00,
+        0x65, 0x00, 0x74, 0x00
+    };
+
+    /* CONNECT set message */
+    builder = _mbim_message_command_builder_new (1,
+                                                 MBIM_SERVICE_BASIC_CONNECT,
+                                                 MBIM_CID_BASIC_CONNECT_CONNECT,
+                                                 MBIM_MESSAGE_COMMAND_TYPE_SET);
+    _mbim_message_command_builder_append_guint32 (builder, 0x01);
+    _mbim_message_command_builder_append_guint32 (builder, (guint32)MBIM_ACTIVATION_COMMAND_ACTIVATE);
+    _mbim_message_command_builder_append_string  (builder, "internet");
+    _mbim_message_command_builder_append_string  (builder, "");
+    _mbim_message_command_builder_append_string  (builder, "");
+    _mbim_message_command_builder_append_guint32 (builder, (guint32)MBIM_COMPRESSION_NONE);
+    _mbim_message_command_builder_append_guint32 (builder, (guint32)MBIM_AUTH_PROTOCOL_PAP);
+    _mbim_message_command_builder_append_guint32 (builder, (guint32)MBIM_CONTEXT_IP_TYPE_IPV4);
+    _mbim_message_command_builder_append_uuid    (builder, mbim_uuid_from_context_type (MBIM_CONTEXT_TYPE_INTERNET));
+    message = _mbim_message_command_builder_complete (builder);
+
+    g_assert (message != NULL);
+
+    test_message_trace ((const guint8 *)((GByteArray *)message)->data,
+                        ((GByteArray *)message)->len,
+                        expected_message,
+                        sizeof (expected_message));
+
+    g_assert_cmpuint (mbim_message_get_transaction_id (message), ==, 1);
+    g_assert_cmpuint (mbim_message_get_message_type   (message), ==, MBIM_MESSAGE_TYPE_COMMAND);
+    g_assert_cmpuint (mbim_message_get_message_length (message), ==, sizeof (expected_message));
+
+    g_assert_cmpuint (mbim_message_command_get_service      (message), ==, MBIM_SERVICE_BASIC_CONNECT);
+    g_assert_cmpuint (mbim_message_command_get_cid          (message), ==, MBIM_CID_BASIC_CONNECT_CONNECT);
+    g_assert_cmpuint (mbim_message_command_get_command_type (message), ==, MBIM_MESSAGE_COMMAND_TYPE_SET);
+
+    g_assert_cmpuint (((GByteArray *)message)->len, ==, sizeof (expected_message));
+    g_assert (memcmp (((GByteArray *)message)->data, expected_message, sizeof (expected_message)) == 0);
+
+    mbim_message_unref (message);
+}
+
+static void
+test_message_command_builder_set_connect (void)
+{
+    GError *error = NULL;
+    MbimMessage *message;
+    const guint8 expected_message [] = {
+        /* header */
+        0x03, 0x00, 0x00, 0x00, /* type */
+        0x7C, 0x00, 0x00, 0x00, /* length */
+        0x01, 0x00, 0x00, 0x00, /* transaction id */
+        /* fragment header */
+        0x01, 0x00, 0x00, 0x00, /* total */
+        0x00, 0x00, 0x00, 0x00, /* current */
+        /* command_message */
+        0xA2, 0x89, 0xCC, 0x33, /* service id */
+        0xBC, 0xBB, 0x8B, 0x4F,
+        0xB6, 0xB0, 0x13, 0x3E,
+        0xC2, 0xAA, 0xE6, 0xDF,
+        0x0C, 0x00, 0x00, 0x00, /* command id */
+        0x01, 0x00, 0x00, 0x00, /* command_type */
+        0x4C, 0x00, 0x00, 0x00, /* buffer_length */
+        /* information buffer */
+        0x01, 0x00, 0x00, 0x00, /* session id */
+        0x01, 0x00, 0x00, 0x00, /* activation command */
+        0x3C, 0x00, 0x00, 0x00, /* access string offset */
+        0x10, 0x00, 0x00, 0x00, /* access string size */
+        0x00, 0x00, 0x00, 0x00, /* username offset */
+        0x00, 0x00, 0x00, 0x00, /* username size */
+        0x00, 0x00, 0x00, 0x00, /* password offset */
+        0x00, 0x00, 0x00, 0x00, /* password size */
+        0x00, 0x00, 0x00, 0x00, /* compression */
+        0x01, 0x00, 0x00, 0x00, /* auth protocol */
+        0x01, 0x00, 0x00, 0x00, /* ip type */
+        0x7E, 0x5E, 0x2A, 0x7E, /* context type */
+        0x4E, 0x6F, 0x72, 0x72,
+        0x73, 0x6B, 0x65, 0x6E,
+        0x7E, 0x5E, 0x2A, 0x7E,
+        /* data buffer */
+        0x69, 0x00, 0x6E, 0x00, /* access string */
+        0x74, 0x00, 0x65, 0x00,
+        0x72, 0x00, 0x6E, 0x00,
+        0x65, 0x00, 0x74, 0x00
+    };
+
+    /* CONNECT set message */
+    message = (mbim_message_connect_set_new (
+                   0x01,
+                   MBIM_ACTIVATION_COMMAND_ACTIVATE,
+                   "internet",
+                   "",
+                   "",
+                   MBIM_COMPRESSION_NONE,
+                   MBIM_AUTH_PROTOCOL_PAP,
+                   MBIM_CONTEXT_IP_TYPE_IPV4,
+                   mbim_uuid_from_context_type (MBIM_CONTEXT_TYPE_INTERNET),
+                   &error));
+    g_assert_no_error (error);
+    g_assert (message != NULL);
+    mbim_message_set_transaction_id (message, 1);
+
+    test_message_trace ((const guint8 *)((GByteArray *)message)->data,
+                        ((GByteArray *)message)->len,
+                        expected_message,
+                        sizeof (expected_message));
+
+    g_assert_cmpuint (mbim_message_get_transaction_id (message), ==, 1);
+    g_assert_cmpuint (mbim_message_get_message_type   (message), ==, MBIM_MESSAGE_TYPE_COMMAND);
+    g_assert_cmpuint (mbim_message_get_message_length (message), ==, sizeof (expected_message));
+
+    g_assert_cmpuint (mbim_message_command_get_service      (message), ==, MBIM_SERVICE_BASIC_CONNECT);
+    g_assert_cmpuint (mbim_message_command_get_cid          (message), ==, MBIM_CID_BASIC_CONNECT_CONNECT);
+    g_assert_cmpuint (mbim_message_command_get_command_type (message), ==, MBIM_MESSAGE_COMMAND_TYPE_SET);
+
+    g_assert_cmpuint (((GByteArray *)message)->len, ==, sizeof (expected_message));
+    g_assert (memcmp (((GByteArray *)message)->data, expected_message, sizeof (expected_message)) == 0);
+
+    mbim_message_unref (message);
+}
+
 int main (int argc, char **argv)
 {
     g_test_init (&argc, &argv, NULL);
 
     g_test_add_func ("/libmbim-glib/message/command-builder/raw/set-pin", test_message_command_builder_raw_set_pin);
     g_test_add_func ("/libmbim-glib/message/command-builder/set-pin",     test_message_command_builder_set_pin);
+
+    g_test_add_func ("/libmbim-glib/message/command-builder/raw/set-connect", test_message_command_builder_raw_set_connect);
+    g_test_add_func ("/libmbim-glib/message/command-builder/set-connect",     test_message_command_builder_set_connect);
 
     return g_test_run ();
 }
