51c11418eea6368cbc6db74a4bdb19ab8d3b8b6e
Aleksander Morgado <aleksander@lanedo.com>
2013-05-15 17:51:50 +0200

    build: better check for top-level source directory

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,8 @@ test -z "$srcdir" && srcdir=.
 REQUIRED_AUTOMAKE_VERSION=1.9
 PKG_NAME=libmbim
 
-(test -f $srcdir/configure.ac) || {
+(test -f $srcdir/configure.ac \
+  && test -f $srcdir/libmbim-glib/libmbim-glib.h) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
