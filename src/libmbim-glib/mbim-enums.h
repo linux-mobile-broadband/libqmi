b86066de7d5677a0e99724b03e17bf2622ac7d80
Ben Chan <benchan@chromium.org>
2014-02-18 11:27:33 -0800

    libmbim-glib: fix typo in MbimRegistrationFlag
    This patch renames 'MBIM_REGISTRATION_FLAG_MANUAL_PACKET_SERVICE_AUTOMATIC_ATTACH' in
    MbimRegistrationFlag to 'MBIM_REGISTRATION_FLAG_PACKET_SERVICE_AUTOMATIC_ATTACH' to be
    consistent with the MBIM specification.

--- src/libmbim-glib/mbim-enums.h
+++ src/libmbim-glib/mbim-enums.h
@@ -449,14 +449,14 @@ typedef enum {
  * MbimRegistrationFlag:
  * @MBIM_REGISTRATION_FLAG_NONE: None.
  * @MBIM_REGISTRATION_FLAG_MANUAL_SELECTION_NOT_AVAILABLE: Network doesn't support manual network selection.
- * @MBIM_REGISTRATION_FLAG_MANUAL_PACKET_SERVICE_AUTOMATIC_ATTACH: Modem should auto-attach to the network after registration.
+ * @MBIM_REGISTRATION_FLAG_PACKET_SERVICE_AUTOMATIC_ATTACH: Modem should auto-attach to the network after registration.
  *
  * Registration flags.
  */
 typedef enum {
-    MBIM_REGISTRATION_FLAG_NONE                                   = 0,
-    MBIM_REGISTRATION_FLAG_MANUAL_SELECTION_NOT_AVAILABLE         = 1 << 0,
-    MBIM_REGISTRATION_FLAG_MANUAL_PACKET_SERVICE_AUTOMATIC_ATTACH = 1 << 2,
+    MBIM_REGISTRATION_FLAG_NONE                            = 0,
+    MBIM_REGISTRATION_FLAG_MANUAL_SELECTION_NOT_AVAILABLE  = 1 << 0,
+    MBIM_REGISTRATION_FLAG_PACKET_SERVICE_AUTOMATIC_ATTACH = 1 << 2,
 } MbimRegistrationFlag;
 
 /*****************************************************************************/
