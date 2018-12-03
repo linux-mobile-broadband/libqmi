0f439dcf9fba85a5892574cfa906aa666e7f60bf
Eric Caruso <ejcaruso@chromium.org>
2018-12-03 13:41:03 -0800

    qmi-device: small style fix
    Remove a redundant comment and add a space between arguments.

--- src/libqmi-glib/qmi-device.c
+++ src/libqmi-glib/qmi-device.c
@@ -1242,9 +1242,8 @@ qmi_device_release_client (QmiDevice *self,
 
         /* And now, really try to release the CID */
         input = qmi_message_ctl_release_cid_input_new ();
-        qmi_message_ctl_release_cid_input_set_release_info (input, service,cid, NULL);
+        qmi_message_ctl_release_cid_input_set_release_info (input, service, cid, NULL);
 
-        /* And now, really try to release the CID */
         qmi_client_ctl_release_cid (self->priv->client_ctl,
                                     input,
                                     timeout,
