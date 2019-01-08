a68d54ba609c2bd34f9a8be05a814a9414ca2bac
Aleksander Morgado <aleksander@aleksander.es>
2019-01-08 13:51:00 +0100

    build: bump copyrights to 2019

--- src/mbimcli/mbimcli.c
+++ src/mbimcli/mbimcli.c
@@ -172,7 +172,7 @@ print_version_and_exit (void)
 {
     g_print ("\n"
              PROGRAM_NAME " " PROGRAM_VERSION "\n"
-             "Copyright (C) 2013-2018 Aleksander Morgado\n"
+             "Copyright (C) 2013-2019 Aleksander Morgado\n"
              "License GPLv2+: GNU GPL version 2 or later <http://gnu.org/licenses/gpl-2.0.html>\n"
              "This is free software: you are free to change and redistribute it.\n"
              "There is NO WARRANTY, to the extent permitted by law.\n"
