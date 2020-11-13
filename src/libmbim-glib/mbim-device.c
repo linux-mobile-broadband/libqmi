f007997cc15494aac4791bc13b24bb118e6cdc91
Aleksander Morgado <aleksander@aleksander.es>
2020-11-13 11:53:36 +0100

    libmbim-glib,device: don't warn if descriptors file cannot be read
    This is possibly just because we're not using cdc_mbim, it shouldn't
    be a reason to warn anything.

--- src/libmbim-glib/mbim-device.c
+++ src/libmbim-glib/mbim-device.c
@@ -877,8 +877,13 @@ read_max_control_transfer (MbimDevice *self)
     /* Build descriptors filepath */
     descriptors_path = get_descriptors_filepath (self);
     if (!descriptors_path) {
-        g_warning ("[%s] Couldn't get descriptors file path",
-                   self->priv->path_display);
+        /* If descriptors file doesn't exist, it's probably because we're using
+         * some other kernel driver, not the cdc_wdm/cdc_mbim pair, so fallback to
+         * the default and avoid warning about it. */
+        g_debug ("[%s] Couldn't find descriptors file, possibly not using cdc_mbim",
+                 self->priv->path_display);
+        g_debug ("[%s] Fallback to default max control message size: %u",
+                 self->priv->path_display, MAX_CONTROL_TRANSFER);
         return MAX_CONTROL_TRANSFER;
     }
 
