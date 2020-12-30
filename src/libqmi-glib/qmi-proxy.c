4e06a0f5e7abf9eae2c7f81f43205561d429c309
Madhavadas Pottekkat <madhavadas@google.com>
2020-12-30 14:10:49 +0000

    qmi-proxy: Validate qmi_client_info_array before accessing its length.
    (cherry picked from commit a3f6daf14f05b6f001332e2b56765493a877cddc)

--- src/libqmi-glib/qmi-proxy.c
+++ src/libqmi-glib/qmi-proxy.c
@@ -226,7 +226,7 @@ disown_not_released_clients (QmiProxy *self,
 {
     guint i;
 
-    if (!client->qmi_client_info_array->len)
+    if (!client->qmi_client_info_array || !client->qmi_client_info_array->len)
         return;
 
     for (i = 0; i < client->qmi_client_info_array->len; i++) {
@@ -341,6 +341,9 @@ indication_cb (QmiDevice *device,
                Client *client)
 {
     guint i;
+
+    if (!client->qmi_client_info_array)
+        return;
 
     for (i = 0; i < client->qmi_client_info_array->len; i++) {
         QmiClientInfo *info;
@@ -571,6 +574,9 @@ qmi_client_info_array_lookup_cid (GArray     *array,
                                   guint8      cid)
 {
     guint i;
+
+    if (!array)
+        return -1;
 
     for (i = 0; i < array->len; i++) {
         QmiClientInfo *item;
