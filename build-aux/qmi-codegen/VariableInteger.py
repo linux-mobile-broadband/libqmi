4a6c66e3552c353e982c7c6fe451ac4975c48b4c
Aleksander Morgado <aleksander@aleksander.es>
2021-04-26 18:43:59 +0200

    qmi-codegen,gir: all output TLV field readers are optional
    The user can give a NULL pointer if the specific field isn't required.

--- build-aux/qmi-codegen/VariableInteger.py
+++ build-aux/qmi-codegen/VariableInteger.py
@@ -298,7 +298,7 @@ class VariableInteger(Variable):
                          'name'          : variable_name }
 
         template = (
-            '${lp}@${name}: (out): a placeholder for the output #${public_format}, or %NULL if not required.\n')
+            '${lp}@${name}: (out)(optional): a placeholder for the output #${public_format}, or %NULL if not required.\n')
         return string.Template(template).substitute(translations)
 
     """
