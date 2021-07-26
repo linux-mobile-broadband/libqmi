5510ee96105e776d7dfb239f5166a8ff6517228d
Iñigo Martínez <inigomartinez@gmail.com>
2021-07-26 23:12:36 +0200

    build,meson: make bash-completion file install optional
    `qmicli` provides a bash-completion file. To install this file
    `bash-completion`'s pkg-file is checked to set the proper
    installation directory.
    
    This has been made optional.

--- meson_post_install.py
+++ meson_post_install.py
@@ -9,6 +9,9 @@ prefix = os.environ['MESON_INSTALL_DESTDIR_PREFIX']
 bindir = os.path.join(prefix, sys.argv[1])
 subprocess.check_call(['chmod', '755', os.path.join(bindir, 'qmi-network')])
 
-bash_completion_completionsdir = os.path.join(prefix, sys.argv[2])
-os.rename(os.path.join(bash_completion_completionsdir, 'qmicli-completion'),
-          os.path.join(bash_completion_completionsdir, 'qmicli'))
+bash_completion_completionsdir = sys.argv[2]
+if bash_completion_completionsdir:
+    if not os.path.isabs(bash_completion_completionsdir):
+        bash_completion_completionsdir = os.path.join(prefix, bash_completion_completionsdir)
+    os.rename(os.path.join(bash_completion_completionsdir, 'qmicli-completion'),
+              os.path.join(bash_completion_completionsdir, 'qmicli'))
