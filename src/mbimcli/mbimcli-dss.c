10768bda46926f72356fb2cf4315cc74446f551b
Yegor Yefremov <yegorslists@googlemail.com>
2021-07-29 15:13:38 +0200

    mbimcli: replace license boilerplate by SPDX identifiers

--- src/mbimcli/mbimcli-dss.c
+++ src/mbimcli/mbimcli-dss.c
@@ -1,18 +1,7 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
  * mbimcli -- Command line interface to control MBIM devices
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Copyright (C) 2014 NVIDIA CORPORATION
  * Copyright (C) 2014 Aleksander Morgado <aleksander@aleksander.es>
