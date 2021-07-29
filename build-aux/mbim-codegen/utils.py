607497d22b2ef7f2005e356090caf601065c3146
Yegor Yefremov <yegorslists@googlemail.com>
2021-07-29 16:34:29 +0200

    mbim-codegen: replace license boilerplate by SPDX identifiers

--- build-aux/mbim-codegen/utils.py
+++ build-aux/mbim-codegen/utils.py
@@ -1,18 +1,6 @@
 # -*- Mode: python; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
 #
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU Lesser General Public License as published by the Free
-# Software Foundation; either version 2 of the License, or (at your option) any
-# later version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
-# details.
-#
-# You should have received a copy of the GNU Lesser General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc., 51
-# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# SPDX-License-Identifier: LGPL-2.1-or-later
 #
 # Copyright (C) 2012 Lanedo GmbH
 # Copyright (C) 2013 - 2018 Aleksander Morgado <aleksander@aleksander.es>
@@ -31,22 +19,8 @@ def add_copyright(f):
         "\n"
         "/* GENERATED CODE... DO NOT EDIT */\n"
         "\n"
+        "/* SPDX-License-Identifier: LGPL-2.1-or-later */\n"
         "/*\n"
-        " * This library is free software; you can redistribute it and/or\n"
-        " * modify it under the terms of the GNU Lesser General Public\n"
-        " * License as published by the Free Software Foundation; either\n"
-        " * version 2 of the License, or (at your option) any later version.\n"
-        " *\n"
-        " * This library is distributed in the hope that it will be useful,\n"
-        " * but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
-        " * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU\n"
-        " * Lesser General Public License for more details.\n"
-        " *\n"
-        " * You should have received a copy of the GNU Lesser General Public\n"
-        " * License along with this library; if not, write to the\n"
-        " * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,\n"
-        " * Boston, MA 02110-1301 USA.\n"
-        " *\n"
         " * Copyright (C) 2013 - 2018 Aleksander Morgado <aleksander@aleksander.es>\n"
         " */\n"
         "\n");
