c452ffcc6e625340621bec6c03d334cb64ee80ad
Iñigo Martínez <inigomartinez@gmail.com>
2021-09-07 11:11:25 +0200

    build: Rename qmicli completion file
    The qmimcli `bash-completion` file has the `-completion` suffix as
    it can be overriden by the generated qmimcli binary file. However,
    meson builds the binary field in a different build directory so there
    is no need to have it renamed.

--- meson_post_install.py
+++ meson_post_install.py
@@ -11,10 +11,3 @@ prefix = os.environ['MESON_INSTALL_DESTDIR_PREFIX']
 
 bindir = os.path.join(prefix, sys.argv[1])
 subprocess.check_call(['chmod', '755', os.path.join(bindir, 'qmi-network')])
-
-bash_completion_completionsdir = sys.argv[2]
-if bash_completion_completionsdir:
-    if not os.path.isabs(bash_completion_completionsdir):
-        bash_completion_completionsdir = os.path.join(prefix, bash_completion_completionsdir)
-    os.rename(os.path.join(bash_completion_completionsdir, 'qmicli-completion'),
-              os.path.join(bash_completion_completionsdir, 'qmicli'))
