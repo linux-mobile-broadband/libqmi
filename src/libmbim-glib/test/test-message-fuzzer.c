6fa33b465f4815e12a0b1a5b768e0d7c17977b43
Aleksander Morgado <aleksandermj@chromium.org>
2023-11-10 12:40:10 +0000

    libmbim-glib,fuzzer: attempt printable string build with other MBIMEx versions
    As suggested by @rhezashan in https://gitlab.freedesktop.org/mobile-broadband/libmbim/-/issues/42

--- src/libmbim-glib/test/test-message-fuzzer.c
+++ src/libmbim-glib/test/test-message-fuzzer.c
@@ -15,9 +15,9 @@ int
 LLVMFuzzerTestOneInput (const uint8_t *data,
                         size_t         size)
 {
-    g_autofree gchar       *printable = NULL;
-    g_autoptr(MbimMessage)  message = NULL;
-    g_autoptr(GError)       error = NULL;
+    g_autoptr(MbimMessage) message = NULL;
+    g_autoptr(GError)      error = NULL;
+    guint                  mbimex_version_major;
 
     if (!size)
       return 0;
@@ -28,12 +28,16 @@ LLVMFuzzerTestOneInput (const uint8_t *data,
         return 0;
     }
 
-    printable = mbim_message_get_printable_full (message, 1, 0, "---- ", FALSE, &error);
-    if (!printable) {
-        g_printerr ("error: %s\n", error->message);
-        return 0;
-    }
+    /* We support printing as MBIMEx 1, 2 and 3 */
+    for (mbimex_version_major = 1; mbimex_version_major <= 3; mbimex_version_major++) {
+        g_autofree gchar  *printable = NULL;
+        g_autoptr(GError) inner_error = NULL;
 
-    g_print ("%s\n", printable);
+        printable = mbim_message_get_printable_full (message, mbimex_version_major, 0, "---- ", FALSE, &inner_error);
+        if (!printable)
+            g_printerr ("error: %s\n", inner_error->message);
+        else
+            g_print ("%s\n", printable);
+    }
     return 0;
 }
